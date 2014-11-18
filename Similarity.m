a105fcb7d5aa8cfd9d9fb168da4f89d55d25a4ee
Not-Stephen <bkc17@duke.edu>
2014-11-17 20:52:32 -0500

    R squared Value
    Put word that you want to analyze.  Returns an R-squared value of the
    first two data files of that word.

---
+++ Similarity.m
@@ -0,0 +1,16 @@
+function R = Similarity(word)
+load WordData.mat
+currentBatch=WordMapExtended(word);
+F1 = currentBatch{1};
+F2 = currentBatch{2};
+Y1=fftshift(fft(F1));
+Y2=fftshift(fft(F2));
+
+TY1=abs(Y1)./max(abs(Y1(:)));
+TY1=(TY1 > .09).*(TY1);
+TY2=abs(Y2)./max(abs(Y2(:)));
+TY2=(TY2 > .09).*(TY2);
+
+St = sum((TY1 - mean(TY1) ).^2)
+Sr = sum((TY1 - TY2).^2)
+R = (St - Sr) / St
