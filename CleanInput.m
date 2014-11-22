8a8f0c1261441fbc620454b280453a1fcf38403e
mkm4884 <matthew.mccann13@gmail.com>
2014-11-22 18:01:13 -0500

    Minor changes to CleanInput.m

--- CleanInput.m
+++ CleanInput.m
@@ -1,8 +1,7 @@
 %CleanInput.m
-%This file cleans the input signal colledted by WordRecorder.m by setting
-%a 5% of max limit ont he input signal. Any portions of the signal with a
-%magnitude less than 5% of the singal's absolute maxiimum is sent to zero.
-%The original and cleaned signals are visualized
+%This file cleans the input signal collected by WordRecorder.m by setting
+%a 5% of max limit on the input signal. Any portions of the signal with a
+%magnitude less than 5% of the singal's absolute maximum is sent to zero.
 %@author: Matthew McCann (mkm4884)
 
 function currentDataClean = CleanInput(data)
