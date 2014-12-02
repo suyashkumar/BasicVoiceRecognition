756ad99b15a0b3952462deaf71d4569bb46b0f39
ceckersley <ceckersley17@gmail.com>
2014-12-01 22:06:44 -0500

    new manual predictor stuff

---
+++ BasicStatsRunner2.m
@@ -0,0 +1,30 @@
+%% BasicMachineLearningRunner
+
+%% Init
+clear, clc
+fs=44100; % Sample Rate
+rSignal=audiorecorder(fs,16,1); % Recorder to hold signal
+
+%% Get Input
+% file1=input('Type the name of the .mat file with training data: ','s');
+% loadString=strcat('load',{' '},file1);
+% disp(strcat('Loaded ',{' '},file1))
+% eval(loadString{1})
+
+disp('Press Enter to record input...')
+pause
+disp('Recording...')
+recordblocking(rSignal,2);
+inputSignal=getaudiodata(rSignal);
+disp('Thanks')
+
+%inputSignal=CleanInput(inputSignal);
+disp('Playback')
+sound(inputSignal,fs)
+figure(1)
+plot(inputSignal)
+title('Input Signal')
+
+[t]=fullguess(inputSignal);
+fprintf('The Predicted Vowel is "%s" ',t)
+
---
+++ fullguess.m
@@ -0,0 +1,25 @@
+function gguess = fullguess (data)
+load ('MurphyVowelData')
+WordMap = WordMapExtended;
+keys = WordMapExtended.keys();
+[dat,f1] = periodogram(data,[],[],44100,'power');
+for i=1:WordMap.length()
+    currentKey=keys{i}; % get current key
+    currentBatch=WordMap(currentKey);
+    for j=1:length(currentBatch)
+        currentData=currentBatch{j};
+        [vp,fp] = periodogram(currentData,[],[],44100,'power');
+        if i==1
+            gguess = currentKey;
+            mindiff = sum(abs(dat-vp));
+        else
+            newdiff = sum(abs(dat-vp));
+            if newdiff<mindiff
+                mindiff = newdiff;
+                gguess = currentKey;
+            end
+        end
+        
+    end
+end
+
