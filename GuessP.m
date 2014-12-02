c858ab0e111edc823fc16dd89ccb447d30afc0cb
Not-Stephen <bkc17@duke.edu>
2014-12-01 22:07:14 -0500

    Created GuessP.m
    GuessP.m Attempts to use periodogram in the guess.m format.

--- GuessP.m
+++ GuessP.m
@@ -1,33 +1,19 @@
-function [R,Word,W,Rall] = Guess(FftIn)
+function [R,Word,W,Rall] = GuessP(FftIn)
 %%Guess.m
 %%Takes an input and compares it to pre-recorded words.  Compares these
 %%values and estimates the vowel type of the mono-syllabic word.
 %%@author: Brendon Colbert
 
-%Constants
-B1 = 150;
-B2= 350;
-
 %Load Data
-load WordData2.mat
+load WordData.mat
 WordMap=WordMapExtended;
-FftIn=fftshift(fft(FftIn));
-currentF=linspace((-(1/2).*44100),((1/2).*44100),length(FftIn)); % Get the freq vector
-
-%Frequency Component
-FftIn = abs(FftIn)./max(abs(FftIn(:)));
-FftIn = FftIn.^2;
+[FftIn]=periodogram(FftIn);
+plot(FftIn)
+%Normalize
+FftIn = (FftIn)./max((FftIn(:)));
 
 %Cutoff Lower Frequencies
-FftIn = (FftIn.*(FftIn > .2));
-
-%First Bin
-FftIn1 = Binning(FftIn,B1);
-FftIn1 = FftIn1./(max(FftIn1));
-
-%Second Bin
-FftIn2 = Binning(FftIn,B2);
-FftIn2 = FftIn2./(max(FftIn2));
+FftIn = (FftIn.*(FftIn > .20));
 
 keys=WordMap.keys();
 for i=1:WordMap.length()
@@ -35,23 +21,13 @@ for i=1:WordMap.length()
     currentBatch=WordMap(currentKey);
     F2 = currentBatch{1};
     %Transform to Frequency Domain
-    Y2=fftshift(fft(F2));
-    Y2 = Y2.^2;
-    TY2=abs(Y2)./max(abs(Y2(:)));
-    TY2=(TY2.*(TY2 > .2));
+    [Y2x,Y2]=periodogram(F2);
     
-    %First Bin
-    TY21 = Binning(TY2,B1);
-    TY21 = TY21./max(TY21);
+    TY2=(Y2)./max((Y2(:)));
+    TY2=(TY2.*(TY2 > .20));
     
-    %Second Bin
-    TY22 = Binning(TY2,B2);
-    TY22 = TY22./max(TY22);
-    
-    length(FftIn2)
-    length(TY22)
     %Difference Value Calculation
-    Sr(i) = sum((FftIn1 - TY21).^2) + sum((FftIn2 - TY22).^2);
+    Sr(i) = sum((FftIn - TY2).^2)
 end
 
 R = Sr;
@@ -74,12 +50,9 @@ Rall = r;
 currentKey=keys{find(r==R(1))};
 currentBatch=WordMap(currentKey);
 F2 = currentBatch{1};
-Y2=fftshift(fft(F2));
-Y2 = Y2.^2;
-TY2=abs(Y2)./max(abs(Y2(:)));
-TY2=(TY2.*(TY2 > .2));
-TY2 = Binning(TY2,B1);
-TY2 = TY2./(max(TY2));
+[Y2x,Y2]=periodogram(F2);
+TY2=(Y2)./max((Y2(:)));
+TY2=(TY2.*(TY2 > .20));
 
 %Vowel Sound
 W = char(Word(1))
@@ -87,11 +60,13 @@ if W(2) == 'a'
     W = 'a';
 elseif W(2) == 'i'
     W = 'i';
+elseif W(2) == 'u'
+    W = 'u';
 end
 
 figure(2)
-plot(TY2)
+plot(Y2x,TY2)
 hold on
-plot(FftIn1,'g-')
+plot(FftInx,FftIn,'g')
 hold off
 title('Frequency Analysis of Top Result')
